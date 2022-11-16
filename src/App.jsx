9a5033d47d2b0be9937ddddda6b400293591dd96
bobolovecat <youngmaple@qq.com>
2022-11-16 23:20:45 +0800

    feat: aframe-ar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
+// import Header from "./components/Header";
+// import Main from "./components/Main";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Main />
+      {/* <Header />
+      <Main /> */}
+      <a-scene artoolkit>
+        <a-text value="Hello, World!"></a-text>
+        <a-marker-camera preset="Hiro"></a-marker-camera>
+      </a-scene>
     </div>
   );
 }
